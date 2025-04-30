dc31e8b181b868d000b707ca92933569545df203
Micas1001 <154024702+Micas1001@users.noreply.github.com>
2025-04-30 15:17:15 +0100

    Delete website directory

--- website/resources/views/emails/adocao.blade.php
+++
@@ -1,9 +0,0 @@
-<h2>Nova Candidatura de Adoção</h2>
-
-<p><strong>Animal:</strong> {{ $dados['animal'] }} (ID: {{ $dados['animal_id'] }})</p>
-<p><strong>Nome do candidato:</strong> {{ $dados['nome'] }}</p>
-<p><strong>Email:</strong> {{ $dados['email'] }}</p>
-<p><strong>Telefone:</strong> {{ $dados['telefone'] }}</p>
-
-<p><strong>Mensagem:</strong></p>
-<p>{{ $dados['mensagem'] }}</p>
